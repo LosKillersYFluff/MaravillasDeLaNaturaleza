a1c91bfbd925d0d4601ab131b822c957ac3b26b8
Didier Villalaz <51863074+feudo-spec@users.noreply.github.com>
2024-07-21 18:08:25 -0500

    Se puso lindo el xml de Tutorial

--- app/src/main/java/com/example/maravillas/TutorialActivity.java
+++ app/src/main/java/com/example/maravillas/TutorialActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class tutorialActivity extends AppCompatActivity {
+public class TutorialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
