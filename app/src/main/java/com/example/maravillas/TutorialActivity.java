a0bb092ef562e4c560324bd129d930c95d293123
jaiderrico11 <jaidertito11@gmail.com>
2024-07-24 08:29:41 -0500

    Pantalla de caracteristicas

--- app/src/main/java/com/example/maravillas/TutorialActivity.java
+++ app/src/main/java/com/example/maravillas/TutorialActivity.java
@@ -1,24 +1,50 @@
 package com.example.maravillas;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class TutorialActivity extends AppCompatActivity {
+
+    private Button buttonTab1;
+    private Button buttonTab2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tutorial);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonTab1 = findViewById(R.id.boton_reconocer);
+        buttonTab2 = findViewById(R.id.boton_frase);
+
+        buttonTab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTab(new Fragment());
+                buttonTab1.setEnabled(false);
+                buttonTab2.setEnabled(true);
+            }
         });
+
+        buttonTab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTab(new Fragment());
+                buttonTab1.setEnabled(true);
+                buttonTab2.setEnabled(false);
+            }
+        });
+
+        // Show the first tab by default
+        buttonTab1.performClick();
+    }
+
+    private void showTab(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.info, fragment);
+        transaction.commit();
     }
 }
